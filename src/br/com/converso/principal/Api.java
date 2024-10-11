8cbfa36446336378f40d46e1d915c848bce3c38d
GeovannaSSilva <geohsousa13@gmail.com>
2024-10-11 15:34:43 -0300

    primeiro commit

---
+++ src/br/com/converso/principal/Api.java
@@ -0,0 +1,7 @@
+package br.com.converso.principal;
+
+public class Api {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/br/com/converso/principal/Main.java
@@ -0,0 +1,11 @@
+package br.com.converso.principal;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
