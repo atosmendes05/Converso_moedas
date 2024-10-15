a7a1b77c633382408c0e0f072d522fbda2ce4730
atosmendes05 <atosmendes48@gmail.com>
2024-10-15 17:16:47 -0300

    atualizando painel

--- src/br/com/converso/modelos/Painel.java
+++ src/br/com/converso/modelos/Painel.java
@@ -10,7 +10,10 @@ public class Painel {
         Scanner lerOpcao = new Scanner(System.in);
         Scanner lervalor = new Scanner(System.in);
 
-        System.out.println("""
+
+
+       while (true) {
+           System.out.println("""
                 ##############################################################
                 
                 1 - Real --> Dolar
@@ -19,42 +22,53 @@ public class Painel {
                 4 - Dolar --> Euro
                 5 - Libra Esterlina --> Iene Japonês
                 6 - Iene Japonês --> Libra Esterlina
+                7 - Sair
                 
                 ##############################################################
                 """);
 
 
-        System.out.println("digite a opcao desejada:");
-        int opcao = lerOpcao.nextInt();
-        System.out.println("digite o valor de conversao desejado:");
-        double valor = lervalor.nextDouble();
-        double moeda = 0;
-        String mensage = "";
+           System.out.println("digite a opcao desejada:");
+           int opcao = lerOpcao.nextInt();
+           if (opcao == 7){
+               System.out.println("encerrando aplicaçao");
+               break;
+           }else if (opcao > 7){
+               System.out.println("por favor selecione um opcao valida");
+               continue;
+           }
 
-       if (opcao == 1){
-           moeda = minhaApi.BuscaMoeda("BRL").getUsd();
-           mensage = "Valor " + valor + " BRL" + " corresponde ao valor final de: " + valor * moeda + "USD";
-       } else if (opcao == 2) {
-           moeda = minhaApi.BuscaMoeda("USD").getBrl();
-           mensage = "Valor " + valor + " USD" + " corresponde ao valor final de: " + valor * moeda + "BRL";
-       } else if (opcao == 3 ) {
-           moeda = minhaApi.BuscaMoeda("EUR").getUsd();
-           mensage = "Valor " + valor + " corresponde ao valor final de: " + valor * moeda + "USD";
-       } else if (opcao == 4) {
-           moeda = minhaApi.BuscaMoeda("USD").getEur();
-           mensage = "Valor " + valor + " corresponde ao valor final de: " + valor * moeda + "EUR";
-       } else if (opcao == 5) {
-           moeda = minhaApi.BuscaMoeda("GBP").getJpy();
-           mensage = "Valor " + valor + " corresponde ao valor final de: " + valor * moeda + "JPY";
-       } else if (opcao == 6) {
-           moeda = minhaApi.BuscaMoeda("JPY").getGbp();
-           mensage = "Valor " + valor + " corresponde ao valor final de: " + valor * moeda + "GBP";
-       } else {
-           System.out.println("por favor selecione um opcao valida");
+           System.out.println("digite o valor de conversao desejado:");
+           double valor = lervalor.nextDouble();
+
+           double moeda = 0;
+           String mensage = "";
+
+           if (opcao == 1) {
+               moeda = minhaApi.BuscaMoeda("BRL").getUsd();
+               mensage = "Valor " + valor + " BRL" + " corresponde ao valor final de: " + valor * moeda + "USD";
+           } else if (opcao == 2) {
+               moeda = minhaApi.BuscaMoeda("USD").getBrl();
+               mensage = "Valor " + valor + " USD" + " corresponde ao valor final de: " + valor * moeda + "BRL";
+           } else if (opcao == 3) {
+               moeda = minhaApi.BuscaMoeda("EUR").getUsd();
+               mensage = "Valor " + valor + " corresponde ao valor final de: " + valor * moeda + "USD";
+           } else if (opcao == 4) {
+               moeda = minhaApi.BuscaMoeda("USD").getEur();
+               mensage = "Valor " + valor + " corresponde ao valor final de: " + valor * moeda + "EUR";
+           } else if (opcao == 5) {
+               moeda = minhaApi.BuscaMoeda("GBP").getJpy();
+               mensage = "Valor " + valor + " corresponde ao valor final de: " + valor * moeda + "JPY";
+           } else if (opcao == 6) {
+               moeda = minhaApi.BuscaMoeda("JPY").getGbp();
+               mensage = "Valor " + valor + " corresponde ao valor final de: " + valor * moeda + "GBP";
+           }
+
+           System.out.println(mensage);
+
        }
 
 
-        System.out.println(mensage);
 
     }
 
